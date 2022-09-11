a818e8c1c3e1c61be0fb2cce752119deb0b3fae6
JERATEEPS-PC\jerateeps <jerateeps@thaicom.net>
2022-09-11 23:25:23 +0700

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+	public const double FontSize = 22;
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+}
+
+public class GlobleFontSizeExtension : IMarkupExtension
+{
+	public object ProvideValue(IServiceProvider serviceProvider)
+	{
+		return MainPage.FontSize;
+	}
+}
